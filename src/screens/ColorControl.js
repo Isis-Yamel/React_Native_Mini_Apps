10d6baf98747f2f2283d5024ce0c02effd7b0b63
Isis-Yamel <yamelamanca@gmail.com>
2023-01-08 19:38:29 -0500

    * work on colorControler component * manage state with useReducer

--- src/screens/ColorControl.js
+++ src/screens/ColorControl.js
@@ -1,51 +1,60 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View } from 'react-native';
 import ColorControler from '../components/colorControler';
 
+const reducer = (state, action) => {
+  switch (action.colorToChange) {
+    case 'red':
+      return state.red + action.amount > 255 || state.red + action.amount < 0
+        ? state
+        : {...state, red: state.red + action.amount};
+
+    case 'green':
+      return state.green + action.amount > 255 || state.green + action.amount < 0
+        ? state
+        : {...state, green: state.green + action.amount};
+
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue + action.amount < 0
+        ? state
+        : {...state, blue: state.blue + action.amount};
+  
+    default:
+      return state;
+  }
+}
+
 const ColorControl = () => {
-  const [redColor, setRedColor] = useState(0);
-  const [greenColor, setGreenColor] = useState(0);
-  const [blueColor, setBlueColor] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 
+    { 
+      red: 0,
+      green: 0,
+      blue: 0,
+    }
+  );
+  
+  const {red, green, blue} = state;
 
   const COLOR_CHANGE = 15;
-
-  const setColor = (color, change) => {
-    switch (color) {
-      case 'red':
-        redColor + change > 255 || redColor + change < 0 ? null : setRedColor((prevRed) => prevRed + change)
-        break;
-
-      case 'green':
-        greenColor + change > 255 || greenColor + change < 0 ? null : setGreenColor((prevGreen) => prevGreen + change)
-        break;
-
-      case 'blue':
-        blueColor + change > 255 || blueColor + change < 0 ? null : setBlueColor((prevBlue) => prevBlue + change)
-        break;
-
-      default:
-        break;
-    };
-  };
 
   return (
     <View>
       <ColorControler 
-        onDecrease={() => setColor('red', -1 * COLOR_CHANGE)}
-        onIncrease={() => setColor('red', COLOR_CHANGE)}
+        onDecrease={() => dispatch({ colorToChange: 'red', amount: COLOR_CHANGE * -1 })}
+        onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_CHANGE })}
         color="red"
       />
       <ColorControler 
-        onDecrease={() => setColor('green', -1 * COLOR_CHANGE)}
-        onIncrease={() => setColor('green', COLOR_CHANGE)}
+        onDecrease={() => dispatch({ colorToChange: 'green', amount: COLOR_CHANGE * -1 })}
+        onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_CHANGE })}
         color="green"
       />
       <ColorControler 
-        onDecrease={() => setColor('blue', -1 * COLOR_CHANGE)}
-        onIncrease={() => setColor('blue', COLOR_CHANGE)}
+        onDecrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_CHANGE * -1 })}
+        onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_CHANGE })}
         color="blue"
       />
-      <View style={{ width: 100, height: 100, backgroundColor: `rgb(${redColor}, ${greenColor}, ${blueColor})` }} />
+      <View style={{ width: 100, height: 100, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
     </View>
   );
 };
